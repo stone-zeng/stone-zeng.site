863d18aec65dacf719c2029ca02a959d53d52f81
Xiangdong Zeng <xdzeng96@gmail.com>
2019-06-25 20:40:23 +0800

    add hash-tag

--- js/post.js
+++ js/post.js
@@ -1,10 +1,14 @@
 "use strict";
 
-var headerPositions =
-  Array.from(document.querySelectorAll("#content h2, #content h3, #content h4"))
-       .map((e) => e.getBoundingClientRect().top
-                 + window.pageYOffset
-                 - e.ownerDocument.documentElement.clientTop);
+var headers = document.querySelectorAll("#content h2, #content h3, #content h4");
+headers.forEach((e) => { e.innerHTML += `<a href="#${e.id}" class="hash-tag">#</a>`; });
+headers.forEach((e) => {
+  e.onmouseover = () => { e.querySelector(".hash-tag").classList = "hash-tag visible"; };
+  e.onmouseout  = () => { e.querySelector(".hash-tag").classList = "hash-tag"; };
+});
+
+var headerPositions = Array.from(headers).map((e) =>
+  e.getBoundingClientRect().top + window.pageYOffset - e.ownerDocument.documentElement.clientTop);
 
 function sidebarHeader(i) { return "sidebar-header-" + i.toString(); }
 document.querySelectorAll("#sidebar-ul a").forEach((e, i) => { e.id = sidebarHeader(i); });
@@ -17,7 +21,7 @@ function getHeaderIndex(pos) {
 }
 
 window.onscroll = function() {
-  document.querySelectorAll("#sidebar-ul a").forEach((e) => e.className = "inactive");
+  document.querySelectorAll("#sidebar-ul a").forEach((e) => { e.className = "inactive"; });
   var headerIndex = getHeaderIndex(window.pageYOffset);
   if (headerIndex >= 0) {
     document.getElementById(sidebarHeader(headerIndex)).className = "active";
