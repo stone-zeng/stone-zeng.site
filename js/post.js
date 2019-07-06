4b10b76056a5fdaa21d8ee6a5d333bba2b576380
Xiangdong Zeng <xdzeng96@gmail.com>
2019-07-06 21:46:13 +0800

    use eslint

--- js/post.js
+++ js/post.js
@@ -1,29 +1,25 @@
-"use strict";
+'use strict';
 
-var headers = document.querySelectorAll("#content h2, #content h3, #content h4");
-headers.forEach((e) => { e.innerHTML += `<a href="#${e.id}" class="hash-tag">#</a>`; });
-headers.forEach((e) => {
-  e.onmouseover = () => { e.querySelector(".hash-tag").classList = "hash-tag visible"; };
-  e.onmouseout  = () => { e.querySelector(".hash-tag").classList = "hash-tag"; };
-});
-
-var headerPositions = Array.from(headers).map((e) =>
-  e.getBoundingClientRect().top + window.pageYOffset - e.ownerDocument.documentElement.clientTop);
-
-function sidebarHeader(i) { return "sidebar-header-" + i.toString(); }
-document.querySelectorAll("#sidebar-ul a").forEach((e, i) => { e.id = sidebarHeader(i); });
-
-function getHeaderIndex(pos) {
-  for (var i = 0; i < headerPositions.length; i++) {
-    if (headerPositions[i] > pos) { return i - 1; }
-  }
-  return headerPositions.length - 1;
-}
-
-window.onscroll = function() {
-  document.querySelectorAll("#sidebar-ul a").forEach((e) => { e.className = "inactive"; });
-  var headerIndex = getHeaderIndex(window.pageYOffset);
-  if (headerIndex >= 0) {
-    document.getElementById(sidebarHeader(headerIndex)).className = "active";
-  }
-};
+(function (d) {
+  // Add hash-tags
+  const headers         = d.querySelectorAll('#content h2, #content h3, #content h4');
+  const headerPositions = Array.from(headers).map((e) =>
+    e.getBoundingClientRect().top + window.pageYOffset - e.ownerDocument.documentElement.clientTop);
+  headers.forEach((e) => {
+    e.innerHTML += `<a href='#${e.id}' class='hash-tag'>#</a>`;
+    e.onmouseover = () => e.querySelector('.hash-tag').classList = 'hash-tag visible';
+    e.onmouseout  = () => e.querySelector('.hash-tag').classList = 'hash-tag';
+  });
+  // Dynamic sidebar
+  const sidebarHeader = (i) => 'sidebar-header-' + i.toString();
+  const getHeaderIndex = (pos) => {
+    for (let i = 0; i < headerPositions.length; i++) if (headerPositions[i] > pos) return i - 1;
+    return headerPositions.length - 1;
+  };
+  d.querySelectorAll('#sidebar-ul a').forEach((e, i) => e.id = sidebarHeader(i));
+  window.onscroll = () => {
+    d.querySelectorAll('#sidebar-ul a').forEach((e) => e.className = 'inactive');
+    const headerIndex = getHeaderIndex(window.pageYOffset);
+    if (headerIndex >= 0) d.getElementById(sidebarHeader(headerIndex)).className = 'active';
+  };
+})(document);
--- js/script.js
+++ js/script.js
@@ -1,52 +1,44 @@
-"use strict";
+'use strict';
 
-const TEX_LOGO     = "T<span class='e'>e</span>X";
-const LATEX_LA     = "L<span class='a'>a</span>";
-const LATEX_LOGO   = LATEX_LA + TEX_LOGO;
-const LATEX_2E     = "2<span class='epsilon'>ε</span>";
-const TEX_LOGO_MAP = {
-  "TeX":      TEX_LOGO,
-  "LaTeX":    LATEX_LOGO,
-  "LaTeXe":   LATEX_LOGO + "&thinsp;" + LATEX_2E,
-  "LaTeX3":   LATEX_LOGO + "3",
-  "(La)TeX":  "<span class='rkern'>(</span>" + LATEX_LA + "<span class='rparen-kern'>)</span>" + TEX_LOGO,
-  "ConTeXt":  "Co<span class='rkern'>n</span>" + TEX_LOGO + "t",
-  "pdfTeX":   "pdf" + TEX_LOGO,
-  "pdfLaTeX": "pdf" + LATEX_LOGO,
-  "XeTeX":    "X<span class='xe-e xe-e-kern'>e</span>" + TEX_LOGO,
-  "XeLaTeX":  "X<span class='xe-e'>e</span>" + LATEX_LOGO,
-  "LuaTeX":   "Lu<span class='rkern'>a</span>" + TEX_LOGO,
-  "LuaLaTeX": "Lua" + LATEX_LOGO,
-  "upTeX":    "u<span class='rkern'>p</span>" + TEX_LOGO,
-  "upLaTeX":  "up" + LATEX_LOGO,
-  "ApTeX":    "A<span class='rkern'>p</span>" + TEX_LOGO,
-  "CTeX":     "C" + TEX_LOGO,
-  "2e":       LATEX_2E,
-}
-const ZH_PUNCT_MAP = ([
-  [/(——|……)/g,                  "<span class='zh-punct'>$1</span>"],
-  [/([，、：；。！？])([（「《])/g, "<span class='zh-punct-kern'>$1</span>$2"],
-  [/([）」》])([，、：；。！？])/g, "<span class='zh-punct-kern'>$1</span>$2"],
-  [/([）」》])([（「《])/g,         "<span class='zh-lrpunct-kern'>$1</span>$2"],
-  [/^([（「《])/g,                  "<span class='zh-punct-bound'>$1</span>"],
-]);
-
-function texLogoReplacer(match, name, offset, string) {
-  return "<span class='tex-logo'>" + TEX_LOGO_MAP[name] + "</span>";
-}
-
-document.body.querySelectorAll("h2, h3, h4, p, ul, ol, dl, blockquote, figure, table").forEach((e) => {
-  e.innerHTML = e.innerHTML.replace(
-    /\$((?:pdf|Xe|Lua|up|Ap)*(?:La)*TeX[3e]*|\(La\)TeX|ConTeXt|CTeX|2e)\$/g, texLogoReplacer);
-  ZH_PUNCT_MAP.forEach((p) => {
-    e.innerHTML = e.innerHTML.replace(p[0], p[1]);
-  })
-});
-
-document.body.querySelectorAll("h1").forEach((e) => {
-  e.innerHTML = e.innerHTML.replace(
-    /((?:pdf|Xe|Lua|up|Ap)*(?:La)*TeX[3e]*|\(La\)TeX|ConTeXt|CTeX|2e)/g, texLogoReplacer);
-  ZH_PUNCT_MAP.forEach((p) => {
-    e.innerHTML = e.innerHTML.replace(p[0], p[1]);
-  })
-});
+(function (d) {
+  const span = (className, e) => `<span class="${className}">${e}</span>`;
+  const TEX   = `T${span('e', 'e')}X`;
+  const LA    = `L${span('a', 'a')}`;
+  const LATEX = LA + TEX;
+  const TWO_E = `2${span('epsilon', '&epsilon;')}`;
+  const LOGO  = {
+    'TeX':      TEX,
+    'LaTeX':    LATEX,
+    'LaTeXe':   `${LATEX}&thinsp;${TWO_E}`,
+    'LaTeX3':   `${LATEX}3`,
+    '(La)TeX':  `${span('rkern', '(')}${LA}${span('rparen-kern', ')')}${TEX}`,
+    'ConTeXt':  `Co${span('rkern', 'n')}${TEX}t`,
+    'pdfTeX':   `pdf${TEX}`,
+    'pdfLaTeX': `pdf${LATEX}`,
+    'XeTeX':    `X${span('xe-e xe-e-kern', 'e')}${TEX}`,
+    'XeLaTeX':  `X${span('xe-e', 'e')}${LATEX}`,
+    'LuaTeX':   `Lu${span('rkern', 'a')}${TEX}`,
+    'LuaLaTeX': `Lua${LATEX}`,
+    'upTeX':    `u${span('rkern', 'p')}${TEX}`,
+    'upLaTeX':  `up${LATEX}`,
+    'ApTeX':    `A${span('rkern', 'p')}${TEX}`,
+    'CTeX':     `C${TEX}`,
+    '2e':       TWO_E,
+  };
+  const replacePunct = (str) =>
+    str .replace(/(——|……)/g,                      span('zh-punct', '$1'))
+        .replace(/([，、：；。！？])([（『「《〈])/g, span('zh-punct-kern', '$1') + '$2')
+        .replace(/([）』」》〉])([，、：；。！？])/g, span('zh-punct-kern', '$1') + '$2')
+        .replace(/([）』」》〉])([（『「《])/g,       span('zh-lrpunct-kern', '$1') + '$2')
+        .replace(/^([（『「《〈])/g,                  span('zh-punct-bound', '$1'));
+  const replaceLogo = (str) =>
+    str.replace(/\$((?:pdf|Xe|Lua|up|Ap)*(?:La)*TeX[3e]*|\(La\)TeX|ConTeXt|CTeX|2e)\$/g,
+        (match, name) => span('tex-logo', LOGO[name]));
+  const replaceLogoH1 = (str) =>
+    str.replace(/((?:pdf|Xe|Lua|up|Ap)*(?:La)*TeX[3e]*|\(La\)TeX|ConTeXt|CTeX|2e)/g,
+        (match, name) => span('tex-logo', LOGO[name]));
+  d.body.querySelectorAll('h2, h3, h4, p, li, figcaption, td, th').forEach((e) =>
+    e.innerHTML = replacePunct(replaceLogo(e.innerHTML)));
+  d.body.querySelectorAll('h1').forEach((e) =>
+    e.innerHTML = replacePunct(replaceLogoH1(e.innerHTML)));
+})(document);
