085dde5cdb5212035092e3276220e89e7736a4d8
Xiangdong Zeng <xdzeng96@gmail.com>
2019-07-09 16:14:12 +0800

    use a subset of font awesome icons; merge js scripts

--- about.md
+++ about.md
@@ -19,17 +19,20 @@ Major in theoretical physics. Lover of typesetting and $LaTeX$.
 - [Jekyll Minima Theme](https://jekyll.github.io/minima/)
 - [Jekyll Pure Liquid Table of Contents](https://github.com/allejo/jekyll-toc)
 - [Source Han Sans](https://fonts.adobe.com/fonts/source-han-sans-cjk-simplified-chinese)
-- [Sticky Sidebar](https://abouolia.github.io/sticky-sidebar/)
 - [Type is Beautiful](https://thetype.com/)
 - [不蒜子 - 极简网页计数器](https://busuanzi.ibruce.info)
 - [始终 - Liam Huang’s blog](https://liam.page/)
+
+<!--
+- [Sticky Sidebar](https://abouolia.github.io/sticky-sidebar/)
+-->
 
 ## 许可证
 
 若无特殊声明，本站所有文章均按照 [CC BY-SA 4.0 协议](https://creativecommons.org/licenses/by-sa/4.0/)发布。
 
 <div class="cc-by-sa-logo">
-  <i class="fab fa-creative-commons"></i>
-  <i class="fab fa-creative-commons-by"></i>
-  <i class="fab fa-creative-commons-sa"></i>
+  <svg class="icon"><use xlink:href="/assets/icons.svg#creative-commons-brands" /></svg>
+  <svg class="icon"><use xlink:href="/assets/icons.svg#creative-commons-by-brands" /></svg>
+  <svg class="icon"><use xlink:href="/assets/icons.svg#creative-commons-sa-brands" /></svg>
 </div>
--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 'use strict';
 
 (function () {
+  updatePunctLogo();
+
+  // For posts only.
+  if (!(document.querySelector('.post-sidebar') === null)) {
+    const headers = document.querySelectorAll('#content h2, #content h3, #content h4');
+    addHashTags(headers);
+    updateSidebar(headers);
+    updateFootnotes();
+  }
+})();
+
+function updatePunctLogo(d) {
   const span = (className, e) => `<span class="${className}">${e}</span>`;
   const TEX   = `T${span('e', 'e')}X`;
   const LA    = `L${span('a', 'a')}`;
@@ -38,8 +50,84 @@
   const replaceLogoH1 = (str) =>
     str.replace(/((?:pdf|Xe|Lua|up|Ap)*(?:La)*TeX[3e]*|\(La\)TeX|ConTeXt|CTeX|2e)/g,
         (_, name) => span('tex-logo', LOGO[name]));
-  document.body.querySelectorAll('h2, h3, h4, p, li, figcaption, td, th').forEach((e) =>
+  document.querySelectorAll('h2, h3, h4, p, li, figcaption, td, th').forEach((e) =>
     e.innerHTML = replacePunct(replaceLogo(e.innerHTML)));
-  document.body.querySelectorAll('h1').forEach((e) =>
+  document.querySelectorAll('h1').forEach((e) =>
     e.innerHTML = replacePunct(replaceLogoH1(e.innerHTML)));
-})();
+}
+
+function addHashTags(headers) {
+  headers.forEach((e) => {
+    e.innerHTML += `<a href='#${e.id}' class='hash-tag'>#</a>`;
+    e.onmouseover = () => e.querySelector('.hash-tag').classList = 'hash-tag visible';
+    e.onmouseout  = () => e.querySelector('.hash-tag').classList = 'hash-tag';
+  });
+}
+
+function updateSidebar(headers) {
+  const headerPositions = Array.from(headers).map((e) =>
+    e.getBoundingClientRect().top + window.pageYOffset - e.ownerDocument.documentElement.clientTop);
+  const sidebarHeader = (i) => 'sidebar-header-' + i.toString();
+  const getHeaderIndex = (pos) => {
+    for (let i = 0; i < headerPositions.length; i++) if (headerPositions[i] > pos) return i - 1;
+    return headerPositions.length - 1;
+  };
+  document.querySelectorAll('#sidebar-ul a').forEach((e, i) => e.id = sidebarHeader(i));
+  window.onscroll = () => {
+    document.querySelectorAll('#sidebar-ul a').forEach((e) => e.className = 'inactive');
+    const headerIndex = getHeaderIndex(window.pageYOffset);
+    if (headerIndex >= 0) document.getElementById(sidebarHeader(headerIndex)).className = 'active';
+  };
+}
+
+function updateFootnotes() {
+  const footnoteList  = document.querySelector('.footnotes');
+  if (!(footnoteList === null)) {
+    const footnoteItems = getFootnotesItems(footnoteList.querySelectorAll('li'));
+    footnoteList.remove();
+    updateFnref(footnoteItems);
+    updateFnlist(footnoteItems);
+  }
+}
+
+function getFootnotesItems(li) {
+  const footnotes = Array.from(li).reduce((obj, e) => (obj[e.id] = e, obj), {});
+  const makeBackref = (href, count) => {
+    if (count === 1) {
+      return `<span class="backref"><a href="#${href}">^</a></span>`;
+    } else {
+      const hrefList = [href].concat(Array.from({length: count-1}, (_, i) => `${href}:${i+1}`))
+          .map((e, i) => `<a href="#${e}">${String.fromCharCode(97+i)}</a>`);
+      return `<span class="backref">${hrefList.join('')}</span>`;
+    }
+  };
+  const newFootnotes = {};
+  for (const [id, val] of Object.entries(footnotes)) {
+    const text = val.innerHTML.match(/<p>(.+?)&nbsp;<a href="#(.+?)"/)[1];
+    const hrefCount = val.querySelectorAll('a.reversefootnote').length;
+    newFootnotes[id] = {
+      text: text,
+      HTML: `<li id="${id}">${makeBackref(id.replace('fn:', 'fnref:'), hrefCount)}${text}</li>`,
+    };
+  }
+  return newFootnotes;
+}
+
+function updateFnref(footnoteItems) {
+  const makeFnref = (sup, href, num, tooltip) =>
+    `<sup${sup} class="fnref">` +
+      `<a href="#${href}">[${num}]</a>` +
+      `<div class="tooltip">${tooltip}</div>` +
+    `</sup>`;
+  document.querySelectorAll('sup[id^="fnref:"]').forEach((e) => {
+    e.outerHTML = e.outerHTML.replace(
+        /<sup(.+?)><a href="#(.+?)"(.+?)>(.+?)<\/a><\/sup>/g,
+        (_, p1, p2, p3, p4) => makeFnref(p1, p2, p4, footnoteItems[p2].text));
+  });
+}
+
+function updateFnlist(footnoteItems) {
+  document.querySelector('#footnotes').innerHTML =
+    `<ol>${Object.values(footnoteItems).map((e) => e.HTML).join('')}</ol>`;
+}
+
