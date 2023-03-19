b4e561c0cee30dd6af4b554f5e49baf40aac5ab0
Xiangdong Zeng <xdzeng96@gmail.com>
2023-03-20 02:25:28 +0800

    merge postcss config into vitepress config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,6 +5,53 @@ import MarkdownItMultimdTable from 'markdown-it-multimd-table'
 import MarkdownItKaTeX from './lib/markdown-it-katex'
 import type { Theme } from './theme/types'
 
+const themeConfig: Theme.Config = {
+  paginate: 10,
+  editLink: {
+    pattern: 'https://github.com/stone-zeng/stone-zeng.github.io/blob/vitepress/docs/:path',
+    text: 'Page source',
+  },
+  nav: [
+    { text: 'Archive', link: '/archive' },
+    { text: 'About', link: '/about' },
+  ],
+  footer: {
+    socialLinks: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/stone-zeng',
+        color: { light: '#222222', dark: '#dee2e6' },
+        icon: 'github',
+      },
+      {
+        name: 'Twitter',
+        link: 'https://twitter.com/xiangdong_zeng',
+        color: '#1da1f2',
+        icon: 'twitter',
+      },
+      {
+        name: 'Telegram',
+        link: 'https://t.me/xdzeng96',
+        color: '#0088cc',
+        icon: 'telegram',
+      },
+      {
+        name: 'E-mail',
+        link: 'mailto:xdzeng96@gmail.com',
+        color: '#fbbf24',
+        icon: 'email',
+      },
+      {
+        name: 'RSS',
+        link: '/feed.xml',
+        color: '#f26522',
+        icon: 'rss',
+      },
+    ],
+    copyright: `© 2018\u{2013}${new Date().getFullYear()} Xiangdong Zeng`,
+  },
+}
+
 // See https://github.com/vuejs/vitepress/issues/1067
 const highlighter = async () => {
   const highlighter = await getHighlighter({
@@ -44,9 +91,10 @@ export default async () =>
     rewrites: {
       'posts/:post.md': ':post.md',
     },
-
-    head: [['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }]],
-
+    // prettier-ignore
+    head: [
+      ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+    ],
     markdown: {
       typographer: true,
       headers: true,
@@ -56,59 +104,23 @@ export default async () =>
         md.use(MarkdownItMultimdTable, { headerless: true })
       },
     },
-
-    themeConfig: {
-      paginate: 10,
-      editLink: {
-        pattern: 'https://github.com/stone-zeng/stone-zeng.github.io/blob/vitepress/docs/:path',
-        text: 'Page source',
-      },
-      nav: [
-        { text: 'Archive', link: '/archive' },
-        { text: 'About', link: '/about' },
-      ],
-      footer: {
-        socialLinks: [
-          {
-            name: 'GitHub',
-            link: 'https://github.com/stone-zeng',
-            color: { light: '#222222', dark: '#dee2e6' },
-            icon: 'github',
-          },
-          {
-            name: 'Twitter',
-            link: 'https://twitter.com/xiangdong_zeng',
-            color: '#1da1f2',
-            icon: 'twitter',
-          },
-          {
-            name: 'Telegram',
-            link: 'https://t.me/xdzeng96',
-            color: '#0088cc',
-            icon: 'telegram',
-          },
-          {
-            name: 'E-mail',
-            link: 'mailto:xdzeng96@gmail.com',
-            color: '#fbbf24',
-            icon: 'email',
-          },
-          {
-            name: 'RSS',
-            link: '/feed.xml',
-            color: '#f26522',
-            icon: 'rss',
-          },
-        ],
-        copyright: `© 2018\u{2013}${new Date().getFullYear()} Xiangdong Zeng`,
-      },
-    },
-
     vite: {
+      css: {
+        postcss: {
+          // prettier-ignore
+          plugins: [
+            require('postcss-import'),
+            require('tailwindcss'),
+            require('autoprefixer'),
+          ],
+        },
+      },
       resolve: {
         alias: {
           '@': fileURLToPath(new URL('./', import.meta.url)),
         },
       },
     },
+
+    themeConfig,
   })
