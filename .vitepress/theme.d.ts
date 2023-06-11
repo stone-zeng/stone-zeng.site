fd1d36ee17fb47cb096b946b57225de0ed433d5a
Xiangdong Zeng <xdzeng96@gmail.com>
2023-06-12 03:16:37 +0800

    simplify type declarations

--- .vitepress/theme.d.ts
+++ .vitepress/theme.d.ts
@@ -1,5 +1,5 @@
 declare namespace Theme {
-  export interface Config {
+  interface Config {
     paginate: number
     editLink: EditLink
     nav: NavItem[]
@@ -9,29 +9,29 @@ declare namespace Theme {
     }
   }
 
-  export type EditLink = {
+  type EditLink = {
     pattern: string
     text: string
   }
 
-  export type NavItem = {
+  type NavItem = {
     text: string
     link: string
   }
 
-  export type SocialLink = {
+  type SocialLink = {
     name: string
     link: string
     color: SocialLinkColor
     icon: SocialLinkIcon
   }
 
-  export type SocialLinkColor =
+  type SocialLinkColor =
     | string
     | {
         light: string
         dark: string
       }
 
-  export type SocialLinkIcon = 'email' | 'github' | 'rss' | 'telegram' | 'twitter'
+  type SocialLinkIcon = 'email' | 'github' | 'rss' | 'telegram' | 'twitter'
 }
--- .vitepress/theme/vite-env.d.ts
+++ .vitepress/theme/vite-env.d.ts
@@ -1,5 +1,4 @@
 /// <reference types="vite/client" />
-/// <reference types="../theme" />
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
